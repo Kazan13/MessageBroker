f6f5acdc75632340f81d8263c0307356c34a02e9
ceasar13m <muharlyamovy@gmail.com>
2020-07-22 10:50:50 +0300

    Message broker работает как надо 1.0

--- src/main/java/com/ainur/broker/network/AppRESTController.java
+++ src/main/java/com/ainur/broker/network/AppRESTController.java
@@ -26,6 +26,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+//
 @RestController
 @CrossOrigin(origins = {"*"})
 public class AppRESTController {
